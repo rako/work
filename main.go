efb732cf0335f734274985b50cb55ac335e6746b
Rako <rako@MacBook-Air.local>
2026-06-20 17:03:56 +0900

    コマンド部分はできた

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+// timeはパッケージ名で、time.Timeで型を指定する
+
+func main() {
+	args := os.Args
+	var worktime time.Time
+
+	switch len(args){
+	case 1:
+		fmt.Println("サブコマンドが必要です。'start'か'end'サブコマンドを入力してください")
+
+	case 2:
+		var subcommand string = args[1]
+
+		// string型のswitchの時はダブルクォーテーションにする
+		switch subcommand{
+		case "start":
+			worktime = time.Now()
+			fmt.Println("Work Start!!!")
+
+		case "end":
+			worktime = time.Now()
+			fmt.Println("Work End!!!")
+
+		default:
+			fmt.Println("'start'か'end'サブコマンドを入力してください")
+		}
+
+		fmt.Println(worktime)
+
+	default:
+		fmt.Println("workコマンドとサブコマンドstart/endのみです")
+		return
+	}
+}
